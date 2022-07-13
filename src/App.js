75ddc4fa03922baa1369f92d91bac9761d062e33
Jon Sundin <jsundin91@gmail.com>
2022-07-12 21:46:24 -0700

    deleting HomeNavigation Dropdown menu deleting hardcoded url in TidbitCard.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import { render } from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import HomeNavigation from "./HomeNavigation";
+import TidbitCard from "./TidbitCard";
 // import Button from "react-bootstrap/Button";
 
 const App = () => {
   return (
     <div>
       <HomeNavigation></HomeNavigation>
+      <TidbitCard
+        title="Good Shit"
+        text="Nadal Ravikant's Podcast on 'Good Shit'"
+        url="https://www.instagram.com/marisssaloren/guide/tidbits/18023476921386740/"
+      />
     </div>
   );
 };
--- src/TidbitCard.js
+++ src/TidbitCard.js
@@ -3,14 +3,12 @@ import Card from "react-bootstrap/Card";
 
 function TidbitCard(props) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card bg="dark" text="light" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.text}</Card.Text>
-        <Card.Link href="https://www.instagram.com/marisssaloren/guide/tidbits/18023476921386740/">
-          Card Link
-        </Card.Link>
+        <Card.Link href={props.url}>Link</Card.Link>
       </Card.Body>
     </Card>
   );
